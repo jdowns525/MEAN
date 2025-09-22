94f8d5fc2372d2b69309e5411c28b74ddb6a8370
Josseph Downs <downsjosseph@gmail.com>
2025-09-21 20:40:11 -0500

    Module 3: Render dynamic trip data using HBS and JSON

--- travlr-express/app_server/controllers/travlr.js
+++ travlr-express/app_server/controllers/travlr.js
@@ -1,11 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
 exports.index = (req, res) => {
+  const tripsPath = path.join(__dirname, '../../data/trips.json');
+  const tripsData = JSON.parse(fs.readFileSync(tripsPath, 'utf8'));
+
   res.render('index', {
     title: 'Travlr Getaways',
-    tagline: 'Where your next adventure begins',
-    nav: [
-      { href: '/', label: 'Home' },
-      { href: '/about', label: 'About' },
-      { href: '/contact', label: 'Contact' }
-    ]
+    tagline: 'Find your next adventure',
+    trips: tripsData
   });
 };
